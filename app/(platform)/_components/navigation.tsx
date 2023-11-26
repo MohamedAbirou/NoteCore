f458ee4c620eac6bad093bc4dbeaf80a36420b49
Mohamed Abirou <abiroumohamed58@gmail.com>
2023-11-26 23:22:32 +0100

    glhf

--- app/(platform)/_components/navigation.tsx
+++ app/(platform)/_components/navigation.tsx
@@ -208,11 +208,11 @@ export const Navigation = () => {
             </PopoverContent>
           </Popover>
         </div>
-        <div
+        {/* <div
           onMouseDown={handleMouseDown}
           onClick={resetWidth}
           className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"
-        />
+        /> */}
       </aside>
       <div
         ref={navbarRef}
